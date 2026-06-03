68f753e5fe994c9ab0ab532e807b7c38512da5a6
Aleksey Kurylev <alekseykurylev@yandex.ru>
2026-06-03 19:30:03 +0300

    add migrations

--- migrations/index.ts
+++ migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260602_074537_init from './20260602_074537_init';
+import * as migration_20260603_162838 from './20260603_162838';
 
 export const migrations = [
   {
     up: migration_20260602_074537_init.up,
     down: migration_20260602_074537_init.down,
-    name: '20260602_074537_init'
+    name: '20260602_074537_init',
+  },
+  {
+    up: migration_20260603_162838.up,
+    down: migration_20260603_162838.down,
+    name: '20260603_162838'
   },
 ];

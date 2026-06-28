5cad247345340174a206b6ac7261b923e68fdc89
Aleksey Kurylev <alekseykurylev@yandex.ru>
2026-06-28 11:15:45 +0300

    feat(menu): convert Menu to compound component with Suspense

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -1,11 +1,22 @@
+import { Suspense } from "react";
 import { Banners } from "@/components/banners/banners";
 import { Menu } from "@/components/menu/menu";
+import { Button } from "@/components/ui/button";
+import { ShoppingBasketIcon } from "lucide-react";
 
 export default function Page() {
   return (
     <>
       <Banners />
-      <Menu />
+
+      <Menu.Root>
+        <Suspense fallback={<Menu.ItemsSkeleton />}>
+          <Menu.Items />
+        </Suspense>
+        <Button size="lg">
+          <ShoppingBasketIcon /> Корзина
+        </Button>
+      </Menu.Root>
     </>
   );
 }
--- components/menu/menu.tsx
+++ components/menu/menu.tsx
@@ -1,20 +1,48 @@
+import { ComponentProps } from "react";
 import { getPayloadLocal } from "@/lib/cms/payload-local";
+import { Button } from "@/components/ui/button";
+import { ShoppingBasketIcon } from "lucide-react";
 import { Container } from "../ui/container";
+import { Skeleton } from "../ui/skeleton";
 
-export async function Menu() {
+function MenuRoot({ children, ...props }: ComponentProps<"div">) {
+  return (
+    <div className="sticky py-6" {...props}>
+      <Container>
+        <div className="flex justify-between gap-4 h-10 items-center">{children}</div>
+      </Container>
+    </div>
+  );
+}
+
+async function MenuItems() {
   const payload = await getPayloadLocal();
 
   const menu = await payload.find({ collection: "categories" });
 
   return (
-    <div>
-      <Container>
-        <ul className="flex min-w-0 scroll-fade-x snap-x snap-mandatory scroll-px-1 scrollbar-none gap-3 overflow-x-auto overscroll-x-contain w-full">
-          {menu.docs.map((item) => (
-            <li key={item.id}>{item.name}</li>
-          ))}
-        </ul>
-      </Container>
+    <div className="scroll-fade-x scrollbar-none overflow-x-auto ">
+      <ul className="flex gap-3">
+        {menu.docs.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
+
+function MenuItemsSkeleton() {
+  return (
+    <div className="flex gap-4">
+      {Array.from({ length: 5 }).map((_, index) => (
+        <Skeleton key={index} className="h-4 w-20" />
+      ))}
+    </div>
+  );
+}
+
+export const Menu = {
+  Root: MenuRoot,
+  Items: MenuItems,
+  ItemsSkeleton: MenuItemsSkeleton,
+};

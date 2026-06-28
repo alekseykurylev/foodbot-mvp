ffbebf6fb8faa1d476e84d90836b488cd48eea14
Aleksey Kurylev <alekseykurylev@yandex.ru>
2026-06-28 16:28:12 +0300

    feat(ui): add w-full to Container component

--- components/ui/container.tsx
+++ components/ui/container.tsx
@@ -1,5 +1,7 @@
 import { cn } from "@/lib/utils";
 
 export function Container({ className, ...props }: React.ComponentProps<"div">) {
-  return <div className={cn("mx-auto max-w-7xl px-4 sm:px-6 lg:px-8", className)} {...props} />;
+  return (
+    <div className={cn("mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8", className)} {...props} />
+  );
 }

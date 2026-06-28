7b1d9a881677e5d87a39ec57a6b68121bd7d985e
Aleksey Kurylev <alekseykurylev@yandex.ru>
2026-06-28 09:15:55 +0300

    feat(banners): add carousel banner component

---
+++ components/banners/banners.tsx
@@ -0,0 +1,30 @@
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+export function Banners() {
+  return (
+    <Carousel className="w-full mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <CarouselContent className="-ml-1">
+        {Array.from({ length: 10 }).map((_, index) => (
+          <CarouselItem key={index} className="basis-1/3 lg:basis-1/5 pl-1">
+            <div className="p-1">
+              <Card className="bg-amber-100">
+                <CardContent className="flex aspect-square items-center justify-center p-6">
+                  <span className="text-2xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      {/*<CarouselPrevious />
+      <CarouselNext />*/}
+    </Carousel>
+  );
+}

822ce74475343fab757c05af8ebacb19ae7b9be0
Aleksey Kurylev <alekseykurylev@yandex.ru>
2026-06-28 20:00:33 +0300

    fix(header): make header sticky with white background

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -4,7 +4,7 @@ import { MenuIcon } from "lucide-react";
 
 export function Header() {
   return (
-    <header>
+    <header className="sticky top-0 z-10 bg-white">
       <Container>
         <div className="h-16 flex items-center justify-between gap-4">
           <div className="flex items-center gap-3">

9ac5557c56db2578c946f50d8c41cc5ba2ad41d3
Aleksey Kurylev <alekseykurylev@yandex.ru>
2026-06-28 16:28:25 +0300

    feat(frontend): wrap menu and products in container and add sidebar

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -4,26 +4,36 @@ import { Menu } from "@/components/menu/menu";
 import { Button } from "@/components/ui/button";
 import { ShoppingBasketIcon } from "lucide-react";
 import { Products } from "@/components/products/products";
+import { Container } from "@/components/ui/container";
 
 export default function Page() {
   return (
     <>
       <Banners />
 
-      <Menu.Root>
-        <Suspense fallback={<Menu.ItemsSkeleton />}>
-          <Menu.Items />
-        </Suspense>
-        <Button size="lg">
-          <ShoppingBasketIcon /> Корзина
-        </Button>
-      </Menu.Root>
+      <Container>
+        <Menu.Root>
+          <Suspense fallback={<Menu.ItemsSkeleton />}>
+            <Menu.Items />
+          </Suspense>
+          <Button size="lg">
+            <ShoppingBasketIcon /> Корзина
+          </Button>
+        </Menu.Root>
 
-      <Products.Root>
-        <Suspense fallback={<Products.Skeleton />}>
-          <Products.List />
-        </Suspense>
-      </Products.Root>
+        <div className="grid gap-6 lg:grid-cols-[minmax(0,1fr)_320px] pt-6">
+          <Products.Root>
+            <Suspense fallback={<Products.Skeleton />}>
+              <Products.List />
+            </Suspense>
+          </Products.Root>
+          <div>
+            <aside className="sticky top-20">
+              <div className="aspect-square bg-amber-300">123</div>
+            </aside>
+          </div>
+        </div>
+      </Container>
     </>
   );
 }

517aa5bf123172693e5ff164d012ed95b5a288d4
Aleksey Kurylev <alekseykurylev@yandex.ru>
2026-07-19 17:09:53 +0300

    chore(payload): squash all migrations into a single baseline

--- migrations/index.ts
+++ migrations/index.ts
@@ -1,69 +1,9 @@
-import * as migration_20260602_074537_init from './20260602_074537_init';
-import * as migration_20260603_162838 from './20260603_162838';
-import * as migration_20260603_191406 from './20260603_191406';
-import * as migration_20260604_172940 from './20260604_172940';
-import * as migration_20260605_184746 from './20260605_184746';
-import * as migration_20260606_034408 from './20260606_034408';
-import * as migration_20260614_184540 from './20260614_184540';
-import * as migration_20260620_074051 from './20260620_074051';
-import * as migration_20260621_075426 from './20260621_075426';
-import * as migration_20260718_145225 from './20260718_145225';
-import * as migration_20260719_091434 from './20260719_091434';
+import * as migration_20260719_140719 from './20260719_140719';
 
 export const migrations = [
   {
-    up: migration_20260602_074537_init.up,
-    down: migration_20260602_074537_init.down,
-    name: '20260602_074537_init',
-  },
-  {
-    up: migration_20260603_162838.up,
-    down: migration_20260603_162838.down,
-    name: '20260603_162838',
-  },
-  {
-    up: migration_20260603_191406.up,
-    down: migration_20260603_191406.down,
-    name: '20260603_191406',
-  },
-  {
-    up: migration_20260604_172940.up,
-    down: migration_20260604_172940.down,
-    name: '20260604_172940',
-  },
-  {
-    up: migration_20260605_184746.up,
-    down: migration_20260605_184746.down,
-    name: '20260605_184746',
-  },
-  {
-    up: migration_20260606_034408.up,
-    down: migration_20260606_034408.down,
-    name: '20260606_034408',
-  },
-  {
-    up: migration_20260614_184540.up,
-    down: migration_20260614_184540.down,
-    name: '20260614_184540',
-  },
-  {
-    up: migration_20260620_074051.up,
-    down: migration_20260620_074051.down,
-    name: '20260620_074051',
-  },
-  {
-    up: migration_20260621_075426.up,
-    down: migration_20260621_075426.down,
-    name: '20260621_075426',
-  },
-  {
-    up: migration_20260718_145225.up,
-    down: migration_20260718_145225.down,
-    name: '20260718_145225',
-  },
-  {
-    up: migration_20260719_091434.up,
-    down: migration_20260719_091434.down,
-    name: '20260719_091434'
+    up: migration_20260719_140719.up,
+    down: migration_20260719_140719.down,
+    name: '20260719_140719'
   },
 ];

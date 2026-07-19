f342b791b4b6753bd7d624524bedd2333d84a248
Aleksey Kurylev <alekseykurylev@yandex.ru>
2026-07-19 17:55:31 +0300

    chore(migrations): remove outdated initial migration

--- migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20260719_140719 from './20260719_140719';
-
-export const migrations = [
-  {
-    up: migration_20260719_140719.up,
-    down: migration_20260719_140719.down,
-    name: '20260719_140719'
-  },
-];

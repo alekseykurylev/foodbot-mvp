21b3c53d19d2edd57dc2b7de79fbc949dcdbddb6
Aleksey Kurylev <alekseykurylev@yandex.ru>
2026-07-20 11:14:58 +0300

    feat(payload): add initial database migration

---
+++ migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260720_081437 from './20260720_081437';
+
+export const migrations = [
+  {
+    up: migration_20260720_081437.up,
+    down: migration_20260720_081437.down,
+    name: '20260720_081437'
+  },
+];
